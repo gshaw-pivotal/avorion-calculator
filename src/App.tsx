81fb79373cb8757f261993743abc3ceebd63575e
Gavin Shaw <gshaw+github@pivotal.io>
2023-07-13 16:06:31 -0400

    Add material ui dark theme at root level of application

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import './App.css';
 import AppHeader from "./AppHeader";
 import TabGroupComponent from "./TabGroupComponent";
+import {createTheme, ThemeProvider} from "@mui/material";
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
 function App() {
     return (
         <div className="App">
-            <AppHeader/>
-            <TabGroupComponent/>
+            <ThemeProvider theme={darkTheme}>
+                <AppHeader/>
+                <TabGroupComponent/>
+            </ThemeProvider>
         </div>
     );
 }
